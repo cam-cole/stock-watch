5cc8f36a0f583b9f1ad7cf6993d92073445983cb
cam-cole <camcole065@gmail.com>
2022-04-27 13:38:20 -0500

    got multiple api requests to work

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,8 +40,8 @@ var getStockProfile = function(stockTicker) {
     
 }
 
-var getSavedStockProfiles = function(savedTickers) {
-    for (i=0; i < savedTickers.length; i++) {
+var getSavedStockProfiles = function(savedTickers, i) {
+    setTimeout(function() {
         fetch('https://yh-finance.p.rapidapi.com/stock/v2/get-profile?symbol=' + savedTickers[i] + '&region=US', options).then(function(response) {
             if(response.ok) {
                 response.json().then(function(data) {
@@ -49,13 +49,28 @@ var getSavedStockProfiles = function(savedTickers) {
 
                     var stockName = data.quoteType.longName;
                     var ticker = data.quoteType.symbol;
+                    var currentPrice = data.price.regularMarketPrice.fmt;
+                    var marketChangePrice = data.price.regularMarketChange.fmt;
+                    var marketChangePct = data.price.regularMarketChangePercent.fmt;
 
-                    $('#saved-stock' + (i+1)).text(stockName + " (" + ticker + ")");
+                    $('#saved-stock' + (i + 1)).text(stockName + " (" + ticker + ")");
+                    $('#saved-stock' + (i + 1) + '-price').text("$" + currentPrice);
+                    $('#saved-stock' + (i + 1) + '-market-change').text('$' + marketChangePrice + " (" + marketChangePct + ")");
+
+                    if(marketChangePrice < 0) {
+                        $('#saved-stock' + (i + 1) + '-market-change').removeClass("has-text-white");
+                        $('#saved-stock' + (i + 1) + '-market-change').addClass("has-text-danger");
+                    }
+
+                    else if (marketChangePrice > 0) {
+                        $('#saved-stock' + (i + 1) + '-market-change').removeClass("has-text-white");
+                        $('#saved-stock' + (i + 1) + '-market-change').addClass("has-text-success");
+                    }
                 })
             }
-        })
-        setTimeout(console.log('Fetched stock: ' + i), 5000);
-    }
+        })       
+    }, 300 * i)
+    
 }
 
 // var getStockGraph = function(stockTicker) {
@@ -73,7 +88,9 @@ $('#search').on("click", function() {
     console.log("Search button was clicked!");
     var stockTicker = $("#search-input").val();
     getStockProfile(stockTicker);
-    getSavedStockProfiles(savedTickers);
+    for (i = 0; i < savedTickers.length; i++) {
+        getSavedStockProfiles(savedTickers, i);
+    }
     // getStockGraph(stockTicker);
 })
 
